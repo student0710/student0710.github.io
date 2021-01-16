4be078d0e9dcdb018e69ffdaed6d1db4e9477d2b
student0710 <75673201+student0710@users.noreply.github.com>
2021-01-17 05:45:43 +0900

    Add files via upload

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,66 +1,68 @@
 // Signup Form.
 (function () {
 
-    // Vars.
-    var $form = document.querySelectorAll('#passwd-form')[0]
-        , $submit = document.querySelectorAll('#passwd-form input[type="submit"]')[
-            0]
-        , $message;
+	// Vars.
+	var $form = document.querySelectorAll('#passwd-form')[0]
+		, $submit = document.querySelectorAll('#passwd-form input[type="submit"]')[
+			0]
+		, $message;
 
-    // Bail if addEventListener isn't supported.
-    if (!('addEventListener' in $form))
-        return;
+	// Bail if addEventListener isn't supported.
+	if (!('addEventListener' in $form))
+		return;
 
-    // Message.
-    $message = document.createElement('span');
-    $message.classList.add('message');
-    $form.appendChild($message);
+	// Message.
+	$message = document.createElement('span');
+	$message.classList.add('message');
+	$form.appendChild($message);
 
-    $message._show = function (type, text) {
+	$message._show = function (type, text) {
 
-        $message.innerHTML = text;
-        $message.classList.add(type);
-        $message.classList.add('visible');
+		$message.innerHTML = text;
+		$message.classList.add(type);
+		$message.classList.add('visible');
 
-        window.setTimeout(function () {
-            $message._hide();
-        }, 1000);
+		window.setTimeout(function () {
+			$message._hide();
+		}, 1000);
 
-    };
+	};
 
-    $message._hide = function () {
-        $message.classList.remove('visible');
-    };
+	$message._hide = function () {
+		$message.classList.remove('visible');
+	};
 
-    // Events.
-    // Note: If you're *not* using AJAX, get rid of this event listener.
-    $form.addEventListener('submit', function (event) {
+	// Events.
+	// Note: If you're *not* using AJAX, get rid of this event listener.
+	$form.addEventListener('submit', function (event) {
 
-        event.stopPropagation();
-        event.preventDefault();
+		event.stopPropagation();
+		event.preventDefault();
 
-        // Hide message.
-        $message._hide();
+		// Hide message.
+		$message._hide();
 
-        // Disable submit.
-        $submit.disabled = true;
+		// Disable submit.
+		$submit.disabled = true;
 
-        // Process form.
-        // Note: Doesn't actually do anything yet (other than report back with a "thank you"),
-        // but there's enough here to piece together a working AJAX submission call that does.
-        window.setTimeout(function () {
+		// Process form.
+		// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
+		// but there's enough here to piece together a working AJAX submission call that does.
+		window.setTimeout(function () {
 
-            const passwd = '00';
-            // Show message.
-            if ($submit === passwd) {
-                $message._show('success', '成功しました。3秒後にメインページに移動します');
-            } else {
-                $message._show('failure', 'パスワードの認証に失敗しました');
-                $submit.disabled = false;
-                $form.reset();
-            }
-        }, 0);
+            const passwd = 'masakimaron0710';
+			// Show message.
+			if ($message == passwd) {
+				$message._show('成功', '成功しました。3秒後にメインページに移動します');
+				$submit.disabled = false;
+				$form.reset();
+			} else {
+				$message._show('失敗', 'パスワードの認証に失敗しました');
+				$submit.disabled = false;
+				$form.reset();
+			}
+		}, 0);
 
-    });
+	});
 
 })();
