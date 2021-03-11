e23800f71fc2ae14a63e36cc10d372179aa051f0
student0710 <dropbox.movie.0710@gmail.com>
2021-03-11 21:55:13 +0900

    軽量化

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,14 +37,11 @@
 			// Images (in the format of 'url': 'alignment').
 			images: {
 				'images/pic01.jpg': 'center',
-				'images/1.jpg': 'center',
-				'images/2.jpg': 'center',
 				'images/3.jpg': 'center',
+				'images/1.jpg': 'center',
 				'images/4.jpg': 'center',
 				'images/pic02.jpg': 'center',
-				'images/pic03.jpg': 'center',
-				'images/pic04.jpg': 'center',
-				'images/pic05.jpg': 'center'
+				'images/pic04.jpg': 'center'
 			},
 
 			// Delay.
